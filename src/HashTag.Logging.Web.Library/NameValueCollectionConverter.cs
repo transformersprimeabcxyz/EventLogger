cc418cc21ae0e046366493f688a51246648672b5
Steve Powell <hashtagdotnet@gmail.com>
2015-05-15 22:48:30 -0400

    --

---
+++ src/HashTag.Logging.Web.Library/NameValueCollectionConverter.cs
@@ -0,0 +1,68 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashTag.Logging.Web.Library
+{
+    internal class NameValuePair
+    {
+        public string Name { get; set; }
+        public string Value { get; set; }
+    }
+
+    public class NameValueCollectionConverter : JsonConverter
+    {
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            if (!(value is NameValueCollection))
+            {
+                return;
+            }
+
+            var collection = (NameValueCollection)value;
+            var container = collection.AllKeys.Select(key => new NameValuePair
+            {
+                Name = key,
+                Value = collection[key]
+            }).ToList();
+
+            var serialized = JsonConvert.SerializeObject(container);
+
+            writer.WriteRawValue(serialized);
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object originalValue, JsonSerializer serializer)
+        {
+            return reader.Value;
+        }
+
+        public override bool CanConvert(Type objectType)
+        {
+            var t = (IsNullableType(objectType))
+                        ? Nullable.GetUnderlyingType(objectType)
+                        : objectType;
+
+            return typeof(NameValueCollection).IsAssignableFrom(t);
+        }
+
+        public static bool IsNullable(Type type)
+        {
+            return type != null && (!type.IsValueType || IsNullableType(type));
+        }
+
+        public static bool IsNullableType(Type type)
+        {
+            if (type == null)
+            {
+                return false;
+            }
+
+            return (type.IsGenericType &&
+                    type.GetGenericTypeDefinition() == typeof(Nullable<>));
+        }
+    }
+}
